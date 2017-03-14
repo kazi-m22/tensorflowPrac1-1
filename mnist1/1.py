65c3a87b7cd0a794886c0a753b75c66e0c995aad
rdeepc <dpr0413@gmail.com>
2017-03-15 00:28:01 +0600

    1

---
+++ mnist1/1.py
@@ -0,0 +1,6 @@
+from tensorflow.examples.tutorials.mnist import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+import matplotlib.pyplot as plt
+import numpy as np
+import random as ran
