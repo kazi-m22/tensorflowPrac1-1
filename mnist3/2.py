39497403d740d15a337e565336b9245eb6a0826a
rdeepc <dpr0413@gmail.com>
2017-03-19 03:26:56 +0600

    15

--- mnist3/2.py
+++ mnist3/2.py
@@ -11,13 +11,13 @@ x_train=mnist.train.images[:num,:]
 y_train=mnist.train.labels[:num,:]
 
 
-x = tf.placeholder(tf.float32, shape=[None, 784],name='x')
+x = tf.placeholder(tf.float32, shape=[None, 784],name='input_m')
 y_ = tf.placeholder(tf.float32, shape=[None, 10],name='y_')
 
 w = tf.Variable(tf.zeros([784,10]),name='w')
 b = tf.Variable(tf.zeros([10]),name='b')
 
-y = tf.nn.softmax(tf.matmul(x,w) + b)
+y = tf.nn.softmax(tf.matmul(x,w) + b,name='output_m')
 
 loss = -tf.reduce_sum(y_ * tf.log(y))
 
@@ -25,25 +25,31 @@ train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
 sess = tf.Session()
 
-init = tf.global_variables_initializer()
-sess.run(init)
+sess.run(tf.global_variables_initializer())
 
 for i in range(5000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     train_step.run({x: batch_xs, y_: batch_ys}, sess)
 
+
 _w=w.eval(sess)
 _b=b.eval(sess)
 
-# print(_w ,_b)
-
 sess.close()
 
-#exporting
+# # Create new graph for exporting
+g_2 = tf.Graph()
+with g_2.as_default():
+    x_2 = tf.placeholder("float", [None, 784], name="input")
+    y_2 = tf.nn.softmax(tf.matmul(x_2, _w) + _b, name="output")
 
-x_m = tf.placeholder(tf.float32, shape=[None, 784], name='input')
-y_m = tf.nn.softmax(tf.matmul(x_m,_w) + _b,name="output")
+    sess_2 = tf.Session()
+    sess_2.run(tf.global_variables_initializer())
 
-sess_m = tf.Session()
-sess_m.run(tf.global_variables_initializer())
-tf.train.write_graph(sess_m.graph,'./model','mnistmodel.pb')
+    tf.train.write_graph(g_2.as_graph_def(), './model','mn.pb', as_text=False)
+
+
+# x_test = mnist.test.images[1:2,:]
+#
+# answer = sess.run(y, feed_dict={x: x_test})
+# print(answer.argmax())
--- mnist3/3.py
+++ mnist3/3.py
@@ -1,3 +1,25 @@
+from tensorflow.examples.tutorials.mnist import input_data
 import tensorflow as tf
+from tensorflow.python.platform import gfile
+# mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+#
+# x_test = mnist.test.images[1:2,:]
+# img=x_test[0]
 
-tf.train.im
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+sess= tf.Session()
+print("load graph")
+with gfile.FastGFile("./model/mn.pb",'rb') as f:
+    graph_def = tf.GraphDef()
+    graph_def.ParseFromString(f.read())
+    sess.graph.as_default()
+    tf.import_graph_def(graph_def, name='')
+print("map variables")
+
+x_test = mnist.test.images[0:1, :]
+
+x=tf.get_default_graph().get_tensor_by_name('input:0')
+y_test=tf.get_default_graph().get_tensor_by_name('output:0')
+answer = sess.run(y_test, feed_dict={x: x_test})
+print(answer.argmax())
