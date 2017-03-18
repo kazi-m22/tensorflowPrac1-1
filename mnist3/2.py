1f3f766d53c2db0427ea91d292f0d0a823a7433b
rdeepc <dpr0413@gmail.com>
2017-03-18 22:04:04 +0600

    13

--- mnist3/2.py
+++ mnist3/2.py
@@ -8,26 +8,27 @@ num=5000
 x_train=mnist.train.images[:num,:]
 y_train=mnist.train.labels[:num,:]
 
-g = tf.Graph()
 
-with g.as_default():
-    x = tf.placeholder(tf.float32, shape=[None, 784],name='x')
-    y_ = tf.placeholder(tf.float32, shape=[None, 10],name='y_')
+x = tf.placeholder(tf.float32, shape=[None, 784],name='x')
+y_ = tf.placeholder(tf.float32, shape=[None, 10],name='y_')
 
-    W = tf.Variable(tf.zeros([784,10]),name='w')
-    b = tf.Variable(tf.zeros([10]),name='b')
+w = tf.Variable(tf.zeros([784,10]),name='w')
+b = tf.Variable(tf.zeros([10]),name='b')
 
-    y = tf.nn.softmax(tf.matmul(x,W) + b)
+y = tf.nn.softmax(tf.matmul(x,w) + b)
 
-    loss = -tf.reduce_sum(y_ * tf.log(y))
+loss = -tf.reduce_sum(y_ * tf.log(y))
 
-    train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 
-    sess = tf.Session()
+sess = tf.Session()
 
-    init = tf.global_variables_initializer()
-    sess.run(init)
+init = tf.global_variables_initializer()
+sess.run(init)
 
-    for i in range(5000):
-        batch_xs, batch_ys = mnist.train.next_batch(100)
-        train_step.run({x: batch_xs, y_: batch_ys}, sess)
+for i in range(5000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    train_step.run({x: batch_xs, y_: batch_ys}, sess)
+
+_w=w.eval(sess)
+_b=b.eval(sess)
